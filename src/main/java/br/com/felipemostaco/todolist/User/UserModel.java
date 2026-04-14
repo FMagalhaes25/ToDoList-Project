b5f8203ced90e31776e79e808b0f92c380682928
FMagalhaes25 <felipemostaco05@gmail.com>
2026-04-14 15:09:43 -0300

    feat: user model

---
+++ src/main/java/br/com/felipemostaco/todolist/User/UserModel.java
@@ -0,0 +1,12 @@
+package br.com.felipemostaco.todolist.User;
+
+import lombok.Data;
+
+@Data
+public class UserModel {
+    
+    private String username;
+    private String name;
+    private String password;
+
+}
