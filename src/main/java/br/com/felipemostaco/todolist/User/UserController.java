0343a116fcefa68b97195eb6ba3f1f50c16c263e
FMagalhaes25 <felipemostaco05@gmail.com>
2026-04-14 16:29:30 -0300

    feat: password with hash and error treatment

--- src/main/java/br/com/felipemostaco/todolist/User/UserController.java
+++ src/main/java/br/com/felipemostaco/todolist/User/UserController.java
@@ -1,9 +1,14 @@
 package br.com.felipemostaco.todolist.User;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
 
 /**
  * Modificador
@@ -14,7 +19,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/users")
 public class UserController {
-    
+
+    @Autowired
+    private IUserRepository userRepository;
+
     /**
      * String
      * Integer
@@ -25,7 +33,21 @@ public class UserController {
      * void
      */
     @PostMapping("/")
-    public void create(@RequestBody UserModel userModel) {
-        System.out.println(userModel.getUsername());
+    public ResponseEntity create(@RequestBody UserModel userModel) {
+        var user = this.userRepository.findByUsername(userModel.getUsername());
+
+        if (user != null) {
+            //Mensagem de erro
+            //Status code (200, 500 etc)
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
+        }
+
+
+        var passwordHashred = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
+
+        userModel.setPassword(passwordHashred);
+
+        var userCreated = this.userRepository.save(userModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
 }
