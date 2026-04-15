8dc2e6d51c14360e294fd1e6bc795fd77ca345c7
FMagalhaes25 <felipemostaco05@gmail.com>
2026-04-14 23:21:50 -0300

    feat: validação do usuario para criação da task

---
+++ src/main/java/br/com/felipemostaco/todolist/filter/FilterTaskAuth.java
@@ -0,0 +1,71 @@
+package br.com.felipemostaco.todolist.filter;
+
+import java.io.IOException;
+import java.util.Base64;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import br.com.felipemostaco.todolist.user.IUserRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class FilterTaskAuth extends OncePerRequestFilter {
+
+    @Autowired
+    private IUserRepository userRepository;
+
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+                
+                var servletPath = request.getServletPath();
+
+                if (servletPath.equals("/tasks")) {
+                    //Pegar a autenticação (usuario e senha)
+                    var authorization = request.getHeader("Authorization");
+
+                    //Retiramos o "Basic" da authorization e colocamos na variavel authEncoded (sobrando somente o hash)
+                    var authEncoded = authorization.substring("Basic".length()).trim();
+
+                    //Decodificamos o authEncoded e inserimos na variavel authDecoded
+                    byte[] authDecoded = Base64.getDecoder().decode(authEncoded);
+
+                    //Transformamos o authDecoded em string para comparar com a senha do usuário
+                    var authString = new String(authDecoded);
+
+                    // ["username123", "password"]
+                    String[] credentials = authString.split(":");
+                    String username = credentials[0];
+                    String password = credentials[1];
+
+                    //Validar usuário
+                    var user = this.userRepository.findByUsername(username);
+                    if (user == null) {
+                        response.sendError(401, "Usuário não encontrado");
+                    } else {
+                        //Validar senha
+                        var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
+                        if (passwordVerify.verified) {
+                            request.setAttribute("idUser", user.getId());
+                            filterChain.doFilter(request, response);
+                        } else {
+                            response.sendError(401);
+                        }
+
+                        //Segue viagem
+
+                    }
+                } else {
+                    filterChain.doFilter(request, response);
+                }             
+    }
+
+
+}
