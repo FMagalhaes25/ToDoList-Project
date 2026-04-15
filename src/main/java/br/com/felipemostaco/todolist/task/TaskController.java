ac661a82761a829813065ab89c2fae904466e4b1
FMagalhaes25 <felipemostaco05@gmail.com>
2026-04-15 00:16:44 -0300

    feat: update da tarefa com corpo completo (PUT)

--- src/main/java/br/com/felipemostaco/todolist/task/TaskController.java
+++ src/main/java/br/com/felipemostaco/todolist/task/TaskController.java
@@ -8,7 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,7 +24,7 @@ public class TaskController {
     @Autowired
     private ITaskRepository taskRepository;
 
-    @PostMapping("")
+    @PostMapping("/")
     public ResponseEntity create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
 
         //Aqui nós setamos o ID do usuario na TaskModel, passando o valor do ID que enviou a request
@@ -47,10 +49,20 @@ public class TaskController {
     }
 
     //Busca as tarefas de acordo com a credencial do usuario
-    @GetMapping("")
+    @GetMapping("/")
     public List<TaskModel> list(HttpServletRequest request) {
         var idUser = request.getAttribute("idUser");
         var tasks = this.taskRepository.findByIdUser((UUID) idUser);
         return tasks;
+    }
+
+    //url/tasks/{taskId} || Buscando o corpo que iremos alterar e o usuario
+    @PutMapping("/{id}")
+    public TaskModel update(@RequestBody TaskModel taskModel, @PathVariable UUID id, HttpServletRequest request) {
+        var idUser = request.getAttribute("idUser");
+        taskModel.setIdUser((UUID) idUser);
+
+        taskModel.setId(id);
+        return this.taskRepository.save(taskModel);
     }
 }
